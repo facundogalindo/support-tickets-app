464fa2512efe21f39e367cce0e11344d9a8b6053
Facundo Galindo <galindofacundo@gmail.com>
2026-03-28 22:11:28 -0300

    todo listo para deploy

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import type { LoginRequest, RegisterRequest, AuthResponse } from "../types/auth";
 
-const API_URL = "http://localhost:3001/auth";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_URL = `${API_BASE_URL}/auth`;
 
 export const loginRequest = async (data: LoginRequest): Promise<AuthResponse> => {
   const response = await axios.post(`${API_URL}/login`, data);
--- frontend/src/services/ticketService.ts
+++ frontend/src/services/ticketService.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import type { Ticket } from "../types/ticket";
 import type { TicketMessage } from "../types/ticketMessage";
 
-const API_URL = "http://localhost:3001/tickets";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_URL = `${API_BASE_URL}/tickets`;
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem("token");
